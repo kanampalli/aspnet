3c001b11fca0d9470074e46c67f7f96c63e55719
unknown <himaja.kanampalli@gmail.com>
2023-07-30 20:43:56 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Greet the user
+            Console.WriteLine("Welcome to the GreetingApp!");
+
+            // Get the current date and time
+            DateTime currentDateTime = DateTime.Now;
+
+            // Display the date and time
+            Console.WriteLine($"Current Date and Time: {currentDateTime}");
+        }
+    }
+}
